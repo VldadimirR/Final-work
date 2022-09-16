5682339322fca02388e5085f696b776883e69c23
VladimirR <Vova.ru0007@yandex.ru>
2022-09-16 14:11:46 +0500

    Финальный коммит

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -25,7 +25,8 @@ void PrintArray(string[] array)
 
         for (int i = 0; i < array.Length; i++)
             {
-                if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+                if (i != array.Length - 1) 
+                Console.Write($"{array[i]}, ");
                 else Console.Write($"{array[i]}.");
             }
         Console.WriteLine();
@@ -36,7 +37,8 @@ int CountStringLessThreeChar(string[] array)
         int count = 0;
         for (int i = 0; i < array.Length; i++)
             {
-                if (array[i].Length < 4) count++;
+                if (array[i].Length < 4) 
+                count++;
             }
         return count;
     }
